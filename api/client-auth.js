0097290073ae849cc50ca191d673597b5c339632
felipedngl <130621269+felipedngl@users.noreply.github.com>
2026-09-03 19:41:48 -0300

    Update client-auth.js

--- api/client-auth.js
+++ api/client-auth.js
@@ -1,6 +1,7 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import crypto from "crypto";
 
 function getFirebaseApp() {
   if (getApps().length > 0) {

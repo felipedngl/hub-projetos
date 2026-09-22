c76770c1ef017d040041996b3f77c9891731df2f
felipedngl <130621269+felipedngl@users.noreply.github.com>
2026-09-22 16:51:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5651,12 +5651,18 @@ async function init() {
       ? await loadProjects()
       : [];
 
+console.log("[INIT] cloudProjects:", cloudProjects);
+console.log("[INIT] quantidade:", cloudProjects?.length);
+	
   if (cloudProjects && cloudProjects.length > 0) {
     projects = cloudProjects.map((p) =>
       typeof seedProject === "function"
         ? seedProject(p)
         : p
     );
+
+console.log("[INIT] projects após carregamento:", projects.length);	  
+	  
   } else if (typeof initialProjects !== "undefined") {
     projects = initialProjects;
   }
